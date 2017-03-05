4067516db9b264d1d2fb798b79bb4497c4138278
Kylie Soderblom <MacDream@Kylies-MacBook-Pro.local>
2017-03-04 23:50:45 -0800

    adjusted view, added no duplicate letters, added random messages

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,79 +1,122 @@
-		function inviteFunction(){
-		var yesplay = confirm("Would you like to play\ Mystic Mumbo?");
-		var name =prompt("What is your name?");
-		if (name!=""){
-		document.getElementById("greeting").innerHTML ="Hello " + name + "! " + "Let's play!";
-		alert("Ok, Let's get started! Choose a letter by clicking the letter on the keyboard."); 
-		}	
-		else{
-			alert("Sayonara");
-		}
-		}
-
-
+		
 		var wins = 0;
 		var loss = 0;
 		var grem = 24; // alphabet 26 -2 starting point 4 guesses
 		var userGuessed=[];
 		var gcount=0;
+		var response =["Ahhhh -- too bad. Try again!","You need to meditate.", "Breathe deeply, and focus.", "Channel the letter gods.", "Feeling frustrated means you're expanding your knowledge","You almost got it right, should've gone with your first choice.", "Believe in yourself."];
+		var lettersOnly = /[a-zA-Z]/; 
 
 
-		var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+		var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 		var compChoice = letters[Math.floor(Math.random()*letters.length)]; 
+		console.log(compChoice);
+		
+
+		function inviteFunction(){
+			var yesplay = confirm("Would you like to play\ Mystic Mumbo?");
+			var name = prompt("What is your name?");
+
+			if (name != "") {
+				document.getElementById("greeting").innerHTML = "Hello " + name + "! " + "Let's play!";
+				alert("Ok, Let's get started! Choose a letter by clicking the letter on the keyboard."); 
+			} else{
+				alert("No name -Sayonara");
+			}
+		};
+
+		// function newCompChoice(){
+		// 	var compChoice = letters[Math.floor(Math.random()*letters.length)]; 
+		// 	console.log(compChoice);
+		// 	return compChoice;
+
+		// }
+
+		function randomResp(){
+			return response[Math.floor(Math.random() * response.length)];
+
+		};
+
+
+		function reset(){
+				userGuessed =[];
+				grem = 24;
+				gcount=0;
+				// newCompChoice();
+				
+			};
+
+
+	
 
 		document.onkeydown=function(event){
-			
-			
+			if((event.keyCode) <65 || (event.keyCode) >122){
+			alert("Please choose a letter from A to Z.");
+		}
+
 			
 		 	var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
 			
-		 if(userChoice != compChoice){
+ 	// 		if(lettersOnly.test(userChoice)=false){   
+		// 	alert("Please choose a letter from A to Z.");
+		// }
+
+			
+			if (userGuessed.includes(userChoice)){
+			 alert("You already guessed that letter");
+			}
+			
+
+		 else if(userChoice != compChoice && grem>2) {
 		 	
-		 
-		 	document.getElementById("greeting").innerHTML ="Ahhhh" + name +" ,you lost." + "You need to focus!";
 		 	userGuessed.push(userChoice);
 		 	userGuessed.sort();
 		 	grem--
-		 	if (grem===0){
-		 		loss++	
-		 	}
-		 	
-		 	gcount++
-		 	document.getElementById("loss").innerHTML ="Losses: " +loss;
-		 	document.getElementById("greeting").innerHTML ="Ahhhh" + name +" ,you lost." + "You need to focus!";
+		 		//random messages
+		 		document.getElementById("greeting").innerHTML =randomResp();
+		 		document.getElementById("grem").innerHTML =grem;
+		 		document.getElementById("gcount").innerHTML ="You guessed: " +[userGuessed];
+		 		document.getElementById("loss").innerHTML ="Losses: " +loss;
+		 		document.getElementById("test").innerHTML =randomResp();
 
-		 }
+		 	}
+		 	// player loses
+		 	else if (userChoice != compChoice && grem<3){
+		 		loss++	
+		 		document.getElementById("greeting").innerHTML = "Oh no! "+ name + "You lost  Try Again";
+		 		document.getElementById("gcount").innerHTML ="You blew it!";
+				console.log("The player just lost");
+		 		// reset();
+		 	}
+	
+		 // player wins
 		  else{ 
-		 	document.getElementById("greeting").innerHTML ="Congratulations" + name +"!" + "You Won!";
+		 	document.getElementById("greeting").innerHTML ="Congratulations" + name +"!" + "You must be psychic!";
 		 	wins++
 		 	document.getElementById("wins").innerHTML ="Wins: "+ wins;
-		 	location.reload(); //hopefully refreshes the page
-		 }
-		}
-/*			keepgoingFunction(){ while (gcount<25) alternatively a For loop{
-			for(var i = 0; userGuessed.length < 25 && i < numbers.length; i++
+		 	document.getElementById("gcount").innerHTML ="Way to go! You aced it!";
+		 	document.getElementById("test").innerHTML ="";
 
-		   CheckingINput
-		 	if userChoice != with a letter in the alphabet or in letters[]{
-			alert("Please choose a letter from A to Z. I don't do symbols.");
+			console.log("The player just won");
+		 	// reset();
 			}
-			else{ 
+		};
 
-		 	}
+			//location.reload(); //lose accum losses/wins
 
-		 How I can put this outside the document onkeydown function and then
-		 when user loses, would also like to add audio.- 
-		 var responses =[
-			{response: "Ahhhh -- too bad. Try again!", result:false}
-			{response: "You need to meditate.", result:false}
-			{response: "Breathe deeply, and focus.", result:false}
-			{response: "Channel my thoughts", result:false}
-			{response: "Feeling frustrated means you're expandinng your knowledge", result:false}
-			{response: "You almost got it right, should've gone with your first choice.", result:false}
-			{response: "Believe in yourself.", result:false}
+/*			
 
-			
-		 ]	
+		reset function 4 new random compChoice but made available globally
+		   
+		CheckingINput
+		line 55:
+			// if(userChoice =(/^[a-zA-Z]+$/)){
+			// alert("Please choose a letter from A to Z.");
+
+			// }
+			or if(userChoice !=([a-zA-Z])){
+
+
 */
 
 
