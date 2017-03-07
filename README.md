25fb8176885513f3032512f9b300cabed15a748a
Kylie Soderblom <MacDream@Kylies-MacBook-Pro.local>
2017-03-06 16:15:32 -0800

    adjusted end game and reset. animation of wins/losses remains

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# PsychicGame
-Week 3 Assignment  Easy
+PsychicGame  
+Title: Mystic Madness
+
+HTML/CSS/Javascript
+
+Simple Guessing Game. User to guess a randomly generated letter in 24 guesses or less. Player notified if they have already guessed a letter. Guessed letters are updated, sorted and shown on the screen. Upon win/loss, accumulative wins and losses are retained while remaining variables are reset to start a new round.
+
+CSS - gradients, wrap-round, floats,
+
+Javascript  
+document.onkeydown=function(event),
+.toUpperCase(),  .toLowerCase(),
+String.fromCharCode(event.keyCode),
+.push(), .sort(), .includes(),
+Math.floor(Math.random()),
+getDocumentById, .innerHTML
+'this'
+
+Note to me: add animation upon win/loss.
+
+CheckingINput
+Alert to user if something other than a letter is chosen.
+if((event.keyCode) <65 || (event.keyCode) >122){
+	alert("Please choose a letter from A to Z.");
+} or something like....
+if(lettersOnly.test(userChoice)=false){   
+	alert("Please choose a letter from A to Z.");
+}
+	
+if(userChoice =(/^[a-zA-Z]+$/)){
+	alert("Please choose a letter from A to Z.");
+}
+or if(userChoice !=([a-zA-Z])){
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,122 +1,107 @@
-		
 		var wins = 0;
 		var loss = 0;
 		var grem = 24; // alphabet 26 -2 starting point 4 guesses
-		var userGuessed=[];
-		var gcount=0;
-		var response =["Ahhhh -- too bad. Try again!","You need to meditate.", "Breathe deeply, and focus.", "Channel the letter gods.", "Feeling frustrated means you're expanding your knowledge","You almost got it right, should've gone with your first choice.", "Believe in yourself."];
-		var lettersOnly = /[a-zA-Z]/; 
+		var userGuessed = [];
+		var gcount = 0;
+		var response = ["Ahhhh -- too bad. Try again!", "You need to meditate.", "Breathe deeply, and focus.", "Channel the letter gods.", "Feeling frustrated means you're expanding your knowledge", "You almost got it right, should've gone with your first choice.", "Believe in yourself."];
+		var lettersOnly = /[a-zA-Z]/;
+		var name = "";
+		var compChoice = null;
 
 
 		var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-		var compChoice = letters[Math.floor(Math.random()*letters.length)]; 
-		console.log(compChoice);
-		
-
-		function inviteFunction(){
-			var yesplay = confirm("Would you like to play\ Mystic Mumbo?");
-			var name = prompt("What is your name?");
-
-			if (name != "") {
-				document.getElementById("greeting").innerHTML = "Hello " + name + "! " + "Let's play!";
-				alert("Ok, Let's get started! Choose a letter by clicking the letter on the keyboard."); 
-			} else{
-				alert("No name -Sayonara");
-			}
-		};
-
-		// function newCompChoice(){
-		// 	var compChoice = letters[Math.floor(Math.random()*letters.length)]; 
-		// 	console.log(compChoice);
-		// 	return compChoice;
-
-		// }
-
-		function randomResp(){
-			return response[Math.floor(Math.random() * response.length)];
-
-		};
+		// var compChoice = letters[Math.floor(Math.random()*letters.length)]; 
 
 
-		function reset(){
-				userGuessed =[];
-				grem = 24;
-				gcount=0;
-				// newCompChoice();
-				
-			};
+		var updateCompChoice = function() {
+		    // Get a random CompChoice letter and assign it based on a random generator
+		    this.compChoice = this.letters[Math.floor(Math.random() * this.letters.length)];
+		    console.log("Computer's Choice has been updated." + compChoice);
 
-
-	
-
-		document.onkeydown=function(event){
-			if((event.keyCode) <65 || (event.keyCode) >122){
-			alert("Please choose a letter from A to Z.");
 		}
 
-			
-		 	var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
-			
- 	// 		if(lettersOnly.test(userChoice)=false){   
-		// 	alert("Please choose a letter from A to Z.");
-		// }
+		function inviteFunction() {
+		    var yesplay = confirm("Would you like to play\ Mystic Madness?");
+		    name = prompt("What is your name?");
 
-			
-			if (userGuessed.includes(userChoice)){
-			 alert("You already guessed that letter");
-			}
-			
+		    if (name != "") {
+		        document.getElementById("greeting").innerHTML = "<br />" + "Hello " + name.toUpperCase() + "! " + "<br />" + "Let's play!";
+		        alert("Ok, Let's get started! Choose a letter by clicking the letter on the keyboard.");
+		        updateCompChoice();
+		        console.log("Computer has chosen:" +compChoice);
+		        document.getElementById("grem").innerHTML = "Guesses left= " + (grem);
 
-		 else if(userChoice != compChoice && grem>2) {
-		 	
-		 	userGuessed.push(userChoice);
-		 	userGuessed.sort();
-		 	grem--
-		 		//random messages
-		 		document.getElementById("greeting").innerHTML =randomResp();
-		 		document.getElementById("grem").innerHTML =grem;
-		 		document.getElementById("gcount").innerHTML ="You guessed: " +[userGuessed];
-		 		document.getElementById("loss").innerHTML ="Losses: " +loss;
-		 		document.getElementById("test").innerHTML =randomResp();
+		        document.getElementById("test").innerHTML = "Hello " + name.toUpperCase() + "! " + "<br />" + "Please choose a letter.";
 
-		 	}
-		 	// player loses
-		 	else if (userChoice != compChoice && grem<3){
-		 		loss++	
-		 		document.getElementById("greeting").innerHTML = "Oh no! "+ name + "You lost  Try Again";
-		 		document.getElementById("gcount").innerHTML ="You blew it!";
-				console.log("The player just lost");
-		 		// reset();
-		 	}
-	
-		 // player wins
-		  else{ 
-		 	document.getElementById("greeting").innerHTML ="Congratulations" + name +"!" + "You must be psychic!";
-		 	wins++
-		 	document.getElementById("wins").innerHTML ="Wins: "+ wins;
-		 	document.getElementById("gcount").innerHTML ="Way to go! You aced it!";
-		 	document.getElementById("test").innerHTML ="";
 
-			console.log("The player just won");
-		 	// reset();
-			}
+		    } else {
+		        alert("No name -Sayonara");
+		    }
 		};
 
-			//location.reload(); //lose accum losses/wins
+		function randomResp() {
+		    return response[Math.floor(Math.random() * response.length)];
 
-/*			
+		};
 
-		reset function 4 new random compChoice but made available globally
-		   
-		CheckingINput
-		line 55:
-			// if(userChoice =(/^[a-zA-Z]+$/)){
-			// alert("Please choose a letter from A to Z.");
-
-			// }
-			or if(userChoice !=([a-zA-Z])){
-
-
-*/
+		var reset = function() {
+		    // name= "";
+		    userGuessed = [];
+		    grem = 24;
+		    gcount = 0;
+		    updateCompChoice();
+		    console.log("The game has been reset. New CompChoice is:" + compChoice);
+		    document.getElementById("test").innerHTML = "Now, I'm thinking of a new letter. Press any key to start a new game";
 
 
+		};
+
+		document.onkeydown = function(event) {
+
+		    var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
+		    console.log("The user chose:" + userChoice)
+		    if (userGuessed.includes(userChoice)) {
+		        alert("You already guessed that letter");
+		    } else if (userChoice != compChoice && grem > 1) {
+
+		        userGuessed.push(userChoice);
+		        userGuessed.sort();
+		        grem--
+		        //random messages
+		        document.getElementById("greeting").innerHTML = randomResp();
+		        document.getElementById("grem").innerHTML = "Guesses left= " + grem;
+		        document.getElementById("gcount").innerHTML = "You guessed: " + "<br />" + [userGuessed];
+		        document.getElementById("loss").innerHTML = "Losses: " + loss;
+		        document.getElementById("test").innerHTML = randomResp();
+
+		    
+		    // player loses
+		    } else if (userChoice != compChoice && grem <= 1) {
+		        loss++
+		        alert("Dagnammit! You lost!");
+		        console.log("The player just lost" + "Number of losses" + loss);
+
+		        document.getElementById("greeting").innerHTML = "Oh no! " + "<br />" + name + "<br />" + "You lost" + "<br />" +  "Try Again!";
+		        document.getElementById("gcount").innerHTML = "You blew it!";
+		        document.getElementById("loss").innerHTML = "Losses: " + loss;
+
+		        reset();
+		        console.log("The game has been reset");
+
+		        // reset();
+		    }
+
+		    // player wins
+		    else {
+		        document.getElementById("greeting").innerHTML = "Congratulations" + "<br />" + name.toUpperCase() + "!" + "<br />" + "You must be psychic!";
+		        wins++
+		       	alert("Wowza! You won!");
+
+		        document.getElementById("wins").innerHTML = "Wins: " + wins;
+		        document.getElementById("gcount").innerHTML = "Way to go!" + "<br />" + "You aced it!";
+		        document.getElementById("test").innerHTML = "Wowza! You won!";
+
+		        console.log("The player just won");
+		        reset();
+		    }
+		};
